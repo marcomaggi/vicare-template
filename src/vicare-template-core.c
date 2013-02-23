f7fbdf41c464289c37acf8fe8cd0465560af76ef
Marco Maggi <marco.maggi-ipsu@poste.it>
2013-02-23 15:43:42 +0100

    added template for C language file for code functions

---
+++ src/vicare-template-core.c
@@ -0,0 +1,51 @@
+/*
+  Part of: Vicare/Template
+  Contents: Template for Vicare
+  Date: Sat Jan 21, 2012
+
+  Abstract
+
+	Core functions.
+
+  Copyright (C) 2013 Marco Maggi <marco.maggi-ipsu@poste.it>
+
+  This program is  free software: you can redistribute  it and/or modify
+  it under the  terms of the GNU General Public  License as published by
+  the Free Software Foundation, either  version 3 of the License, or (at
+  your option) any later version.
+
+  This program  is distributed in the  hope that it will  be useful, but
+  WITHOUT   ANY  WARRANTY;   without  even   the  implied   warranty  of
+  MERCHANTABILITY  or FITNESS  FOR A  PARTICULAR PURPOSE.   See  the GNU
+  General Public License for more details.
+
+  You  should have received  a copy  of the  GNU General  Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+/** --------------------------------------------------------------------
+ ** Headers.
+ ** ----------------------------------------------------------------- */
+
+#include "vicare-template-internals.h"
+
+
+/** --------------------------------------------------------------------
+ ** Still to be implemented.
+ ** ----------------------------------------------------------------- */
+
+#if 0
+ikptr
+ikrt_template-doit (ikpcb * pcb)
+{
+#ifdef HAVE_TEMPLATE_DOIT
+  return IK_VOID;
+#else
+  feature_failure(__func__);
+#endif
+}
+#endif
+
+
+/* end of file */
